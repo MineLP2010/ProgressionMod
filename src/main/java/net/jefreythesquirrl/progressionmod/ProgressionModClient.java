10c089aee7b9ca79ed3c97c20946654bb261bf18
JefreyTheSquirrl <MineLP69@gmail.com>
2025-07-18 11:39:06 +0200

    initial set up changes

---
+++ src/main/java/net/jefreythesquirrl/progressionmod/ProgressionModClient.java
@@ -0,0 +1,10 @@
+package net.jefreythesquirrl.progressionmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProgressionModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
